1ead798ba9601d58e072f341a738b706a77a534c
sitisofiyah <jarakostacity@gmail.com>
2019-01-04 17:46:35 +0700

    Add files via upload

---
+++ menjumlahkan 2buah bilangan.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float bil1,bil2, hasil;
+    cout<<"bilangan 1 :";
+    cin>>bil1;
+    cout<<"bilangan 2 :";
+    cin>>bil2;
+    hasil=bil1+bil2;
+    cout<<"hasil      :"<<hasil;
+    return 0;
+}
